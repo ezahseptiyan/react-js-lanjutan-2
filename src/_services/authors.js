dd4ed1645f973ebe3bdc22a427eee32b4cac97c5
ezahseptiyan <juangastrajinggatidae@gmail.com>
2025-10-29 11:41:22 +0700

    Initial commit

---
+++ src/_services/authors.js
@@ -0,0 +1,40 @@
+import {API} from "../_api"
+
+export const getAuthors = async () => {
+    const { data } = await API.get("/authors")
+    return data.data;
+}
+
+export const createAuthor = async (data) => {
+    try {
+        const response = await API.post("/authors", data)
+        return response.data
+    } catch (error) {
+        console.log(error);
+        throw error
+    }
+}
+
+export const showAuthor = async (id) => {
+    const { data } = await API.get(`/authors/${id}`);
+    return data.data;
+};
+
+export const updateAuthor = async (id, data) => {
+  try {
+    const response = await API.post(`/authors/${id}`, data)
+    return response.data
+  } catch (error) {
+    console.log(error);
+    throw error
+  }
+}
+
+export const deleteAuthor = async (id) => {
+  try {
+    await API.delete(`/authors/${id}`)
+  } catch (error) {
+    console.log(error);
+    throw error
+  }
+}
